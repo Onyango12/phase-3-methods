61679142b1082ced0ab52dde26e2d1559f0ec571
Charles Onyango <charles.onyango@student.moringaschool.com>
2023-05-17 21:53:26 +0300

    complete

--- methods.rb
+++ methods.rb
@@ -1,1 +1,18 @@
 # Your code here!
+# Your code here!
+def  greet_programmer()
+  puts "Hello, programmer!"
+end
+def greet(name)
+  puts "Hello, #{name}! " 
+end
+def greet_with_default(name ="programmer")
+  puts "Hello, #{name}!"
+end
+def add(num1, num2)
+  return num1 + num2
+end
+def halve(num)
+  return nil unless num.is_a?(Integer)
+  return num / 2
+end
